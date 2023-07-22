d6da7a6fb06d38ba62576242be169ba793436deb
Ruon2414 <112769866+Ruon2414@users.noreply.github.com>
2023-07-22 18:28:04 -0400

    Inicio mvc medico

---
+++ views/common/footer.php
@@ -0,0 +1,21 @@
+<div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+  <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+    <div class="text-dark order-2 order-md-1">
+      <span class="text-muted fw-bold me-1">2023 ©</span>
+      <a href="https://stisbolivia.com" target="_blank" class="text-gray-800 text-hover-primary">STIS - BOLIVIA</a>
+    </div>
+    <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+      <li class="menu-item">
+        <a href="#" target="" class="menu-link px-2">INICIO</a>
+      </li>
+      <li class="menu-item">
+        <a href="#" target="" class="menu-link px-2"></a>
+      </li>
+      <li class="menu-item">
+        <div class="text-dark order-2 order-md-1">
+          <span class="text-muted fw-bold me-1">Todos los derechos reservados</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ views/template.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--begin::Head-->
+
+<head>
+  <base href="">
+  <title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel
+    by Keenthemes</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="utf-8" />
+  <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+  <link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+
+  <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+  <!--end::Global Stylesheets Bundle-->
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+
+<body id="kt_body"
+  class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
+  style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+  <!--begin::Main-->
+  <!--begin::Root-->
+  <div class="d-flex flex-column flex-root">
+    <!--begin::Page-->
+    <div class="page d-flex flex-row flex-column-fluid">
+      <!--begin::Aside-->
+      <?php include_once './common/menu.php';?>
+      <!--end::Aside-->
+
+      <!--begin::Wrapper-->
+      <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+        <!--begin::Header-->
+        <?php include_once './common/header.php';?>
+        <!--end::Header-->
+        <!--begin::Content-->
+        <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+          <!-- CONTENIDO -->
+          <!--begin::Toolbar-->
+          <div class="toolbar" id="kt_toolbar">
+            <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
+              <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
+                data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
+                class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Dashboard
+                  <span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
+                  <small class="text-muted fs-7 fw-bold my-1 ms-1">#XRS-45670</small>
+                </h1>
+              </div>
+            </div>
+          </div>
+          <!--end::Toolbar-->
+          <!--begin::Post-->
+          <div class="post d-flex flex-column-fluid" id="kt_post">
+            <!--begin::Container-->
+            <div id="kt_content_container" class="container-xxl">
+              
+            </div>
+            <!--end::Container-->
+          </div>
+          <!--end::Post-->
+        </div>
+        <!--end::Content-->
+        <!--begin::Footer-->
+        <?php include_once './common/footer.php';?>
+        <!--end::Footer-->
+      </div>
+      <!--end::Wrapper-->
+    </div>
+    <!--end::Page-->
+  </div>
+  <!--end::Root-->
+
+  <script>var hostUrl = "assets/";</script>
+  <script src="assets/plugins/global/plugins.bundle.js"></script>
+  <script src="assets/js/scripts.bundle.js"></script>
+  <script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+  <script src="assets/js/custom/widgets.js"></script>
+  <script src="assets/js/custom/apps/chat/chat.js"></script>
+  <script src="assets/js/custom/modals/create-app.js"></script>
+  <script src="assets/js/custom/modals/upgrade-plan.js"></script>
+</body>
+
+</html>
